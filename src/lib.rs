05f33f9498dcc49479af8f8e61d273efe0c22cab
akulij <48361256+akulij@users.noreply.github.com>
2023-01-10 20:47:32 +0500

    changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,140 @@
+mod tools;
+
 pub struct User {
     login: String,
     pasword: String,
 }
 
-pub fn runprog_safemode(user: Option<&User>, program: &str) {
+#[cfg(windows)]
+pub fn runprog_safemode(user: Option<&User>, program_path: &str) {
     if let Some(user) = user {
-        set_autologin(user);
+        if set_autologin(user) {
+            return;
+        }
     }
 
-    set_runonce_program(program);
+    if !set_runonce_program(program_path) {
+        return;
+    }
 
-    set_safemode();
+    if !set_safemode() {
+        return;
+    }
     reboot_pc();
 }
 
+#[cfg(windows)]
 pub fn back_to_normalmode() {
+    if !unset_safemode() {
+        return;
+    }
+    reboot_pc();
+}
+
+fn set_safemode() -> bool {
+    run_bcd("/set safeboot network")
+}
+
+fn unset_safemode() -> bool {
+    run_bcd("/deletevalue safeboot")
+}
+
+fn run_bcd(command: &str) -> bool {
+    use winapi::ctypes::c_void;
+    use winapi::um::shellapi::ShellExecuteA;
+    use winapi::um::winnt::PVOID;
+    use winapi::um::winuser::SW_SHOWNORMAL;
+    use winapi::um::wow64apiset::Wow64DisableWow64FsRedirection;
+    use winapi::um::wow64apiset::Wow64RevertWow64FsRedirection;
+
+    let is_wow_enabled = tools::is_wow64();
+    let mut revert_holder: PVOID = std::ptr::null::<c_void>().cast_mut();
+
+    if is_wow_enabled {
+        unsafe {
+            Wow64DisableWow64FsRedirection(&mut revert_holder);
+        }
+    }
+
+    use tools::cstr;
+    use tools::nullptr;
+
+    unsafe {
+        ShellExecuteA(
+            nullptr(),
+            cstr("runas"),
+            cstr("BCDEdit.exe"),
+            cstr(command),
+            nullptr(),
+            SW_SHOWNORMAL,
+        );
+    }
+
+    if is_wow_enabled {
+        unsafe {
+            Wow64RevertWow64FsRedirection(revert_holder);
+        }
+    }
+
+    true
+}
+
+fn reboot_pc() {
+    use winapi::um::winuser::ExitWindowsEx;
+    use winapi::um::winuser::{EWX_FORCEIFHUNG, EWX_REBOOT};
+
+    unsafe {
+        ExitWindowsEx(EWX_REBOOT, EWX_FORCEIFHUNG);
+    }
+}
+
+fn set_autologin(user: &User) -> bool {
+    set_logon_data("DefaultUserName", user.login.as_str());
+    set_logon_data("DefaultPassword", user.pasword.as_str());
+    set_logon_data("AutoAdminLogon", "1");
+
+    true
+}
+
+fn set_logon_data(value: &str, data: &str) -> bool {
+    use tools::cast_pointer;
+    use tools::encode_str;
+    use winapi::um::winnt::REG_EXPAND_SZ;
+    use winapi::um::winreg::RegSetKeyValueW;
+    use winapi::um::winreg::HKEY_LOCAL_MACHINE;
+
+    unsafe {
+        RegSetKeyValueW(
+            HKEY_LOCAL_MACHINE,
+            encode_str("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon"),
+            encode_str(value),
+            REG_EXPAND_SZ,
+            cast_pointer(encode_str(data)),
+            data.len() as u32 + 1,
+        );
+    }
+
+    true
+}
+
+fn set_runonce_program(path: &str) -> bool {
+    use tools::cast_pointer;
+    use tools::encode_str;
+    use tools::random_code;
+    use winapi::um::winnt::REG_EXPAND_SZ;
+    use winapi::um::winreg::RegSetKeyValueW;
+    use winapi::um::winreg::HKEY_LOCAL_MACHINE;
+
+    unsafe {
+        RegSetKeyValueW(
+            HKEY_LOCAL_MACHINE,
+            encode_str("Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce"),
+            encode_str(random_code(6).as_str()),
+            REG_EXPAND_SZ,
+            cast_pointer(encode_str(path)),
+            path.len() as u32 + 1,
+        );
+    }
+
+    true
 }
---
+++ src/tools.rs
@@ -0,0 +1,75 @@
+pub fn is_wow64() -> bool {
+    use winapi::um::processthreadsapi::GetCurrentProcess;
+    use winapi::um::wow64apiset::IsWow64Process;
+
+    unsafe {
+        let mut is_wow = 0;
+        if IsWow64Process(GetCurrentProcess(), &mut is_wow) == 0 {
+            return false;
+        }
+
+        is_wow != 0
+    }
+}
+
+pub fn cstr(s: &str) -> *const i8 {
+    let c_s = std::ffi::CString::new(s).expect("error creating cstring");
+
+    c_s.as_ptr()
+}
+
+pub fn get_string(s: &str) -> *const i8 {
+    unsafe {
+        let boxed = Box::new(std::ffi::CString::new(s));
+        let data: *const std::ffi::CString = std::mem::transmute(boxed);
+        (&*data).as_ptr()
+    }
+}
+
+pub fn nullptr<T>() -> *mut T {
+    std::ptr::null::<T>().cast_mut()
+}
+
+pub fn encode_str(s: &str) -> *mut u16 {
+    let mut encoded = s.encode_utf16().collect::<Vec<u16>>();
+    encoded.push(0);
+
+    encoded.as_mut_ptr()
+}
+
+pub fn cast_pointer<T, O>(data: *const T) -> *const O {
+    unsafe { std::mem::transmute(data) }
+}
+
+pub fn random_code(lenght: u32) -> String {
+    use rand::seq::SliceRandom;
+    use rand::thread_rng;
+    let alphabet = ('a'..='z').into_iter().collect::<Vec<char>>();
+
+    let mut rand_gen = thread_rng();
+    (0..lenght)
+        .map(|_| {
+            alphabet
+                .choose(&mut rand_gen)
+                .expect("Can't get char from alphabet")
+        })
+        .collect::<String>()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_random_code() {
+        let code = random_code(4);
+
+        println!("Code: {}", code);
+    }
+    #[test]
+    fn test_converter() {
+        let encoded = cstr("aello!");
+
+        println!("Encoded: {:?}", unsafe { *encoded as u8 });
+    }
+}
