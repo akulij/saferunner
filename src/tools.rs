d7d5d7faacbb7da25169109d2e23f0bc4bfbf9ab
akulij <48361256+akulij@users.noreply.github.com>
2023-01-10 23:52:44 +0500

    create tools module with useful functions

---
+++ src/tools.rs
@@ -0,0 +1,50 @@
+#[allow(dead_code)]
+
+pub fn is_wow64() -> bool {
+    use winapi::um::processthreadsapi::GetCurrentProcess;
+    use winapi::um::wow64apiset::IsWow64Process;
+
+    unsafe {
+        let mut is_wow = 0;
+        if IsWow64Process(GetCurrentProcess(), &mut is_wow) == 0 {
+            return false;
+        }
+
+        is_wow != 0
+    }
+}
+
+pub fn cstr(s: &str) -> std::ffi::CString {
+    std::ffi::CString::new(s).expect("error creating cstring")
+}
+
+pub fn nullptr<T>() -> *mut T {
+    std::ptr::null::<T>().cast_mut()
+}
+
+pub fn random_code(lenght: u32) -> String {
+    use rand::seq::SliceRandom;
+    use rand::thread_rng;
+    let alphabet = ('a'..='z').into_iter().collect::<Vec<char>>();
+
+    let mut rand_gen = thread_rng();
+    (0..lenght)
+        .map(|_| {
+            alphabet
+                .choose(&mut rand_gen)
+                .expect("Can't get char from alphabet")
+        })
+        .collect::<String>()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_random_code() {
+        let code = random_code(4);
+
+        assert_eq!(code.len(), 4);
+    }
+}
